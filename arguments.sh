eb60261f8b7f224e851c962d5467e02505f6a9d0
thomasabishop <tactonbishop@gmail.com>
2022-02-08 12:54:58 +0000

    Add script for command line arguments

---
+++ arguments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "File is called $0"
+
+echo "The arguments provided are $@"
+
+echo "The first argument is $1"
+
+echo "The second argument is $2"
+
+echo "Your name is $1 and you are $2 years old"
